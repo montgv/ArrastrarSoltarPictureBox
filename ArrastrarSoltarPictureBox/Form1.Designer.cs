e9dc3744ab7ad4913593c15f3eba7e27565aedb8
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2023-02-05 21:58:26 +0100

    Agregar archivos de proyecto.

---
+++ ArrastrarSoltarPictureBox/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ArrastrarSoltarPictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picBoxIzq = new System.Windows.Forms.PictureBox();
+            this.picBoxDer = new System.Windows.Forms.PictureBox();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIzq)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxDer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxIzq
+            // 
+            this.picBoxIzq.Image = ((System.Drawing.Image)(resources.GetObject("picBoxIzq.Image")));
+            this.picBoxIzq.Location = new System.Drawing.Point(63, 101);
+            this.picBoxIzq.Name = "picBoxIzq";
+            this.picBoxIzq.Size = new System.Drawing.Size(295, 337);
+            this.picBoxIzq.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxIzq.TabIndex = 0;
+            this.picBoxIzq.TabStop = false;
+            this.picBoxIzq.DragDrop += new System.Windows.Forms.DragEventHandler(this.picBoxIzq_DragDrop);
+            this.picBoxIzq.DragEnter += new System.Windows.Forms.DragEventHandler(this.picBoxIzq_DragEnter);
+            this.picBoxIzq.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picBoxIzq_MouseDown);
+            // 
+            // picBoxDer
+            // 
+            this.picBoxDer.Location = new System.Drawing.Point(488, 101);
+            this.picBoxDer.Name = "picBoxDer";
+            this.picBoxDer.Size = new System.Drawing.Size(304, 337);
+            this.picBoxDer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxDer.TabIndex = 1;
+            this.picBoxDer.TabStop = false;
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(145, 39);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(561, 32);
+            this.lbTitulo.TabIndex = 2;
+            this.lbTitulo.Text = "Mover la imagen de un pictureBox a otro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 450);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.picBoxDer);
+            this.Controls.Add(this.picBoxIzq);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxIzq)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxDer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxIzq;
+        private System.Windows.Forms.PictureBox picBoxDer;
+        private System.Windows.Forms.Label lbTitulo;
+    }
+}
+
---
+++ ArrastrarSoltarPictureBox/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrastrarSoltarPictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Nos permite arrastrar la foto al otro pictureBox
+            this.picBoxIzq.AllowDrop = true;
+            this.picBoxDer.AllowDrop = true;
+            //Le asignamos que haga lo mismo que el evento que tiene entre parentesis
+            picBoxDer.MouseDown += new MouseEventHandler(picBoxIzq_MouseDown);
+            picBoxDer.DragEnter += new DragEventHandler(picBoxIzq_DragEnter);
+            picBoxDer.DragDrop += new DragEventHandler(picBoxIzq_DragDrop);
+
+        }
+        //Se produce cuando presionamos el boton izquierdo del raton
+        private void picBoxIzq_MouseDown(object sender, MouseEventArgs e)
+        {
+            PictureBox pictBox = (PictureBox)sender;
+
+            if (pictBox.Image == null)
+            {
+                MessageBox.Show("No hay imagenes.");
+            } else
+            {
+                //Comienza una operacion de arrastrar y soltar
+                pictBox.DoDragDrop(pictBox.Image, DragDropEffects.Copy);
+            }
+        }
+        //Se produce cuando se arrastra la imagen dentro del pictureBox
+        private void picBoxIzq_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+            {
+                //Se muestra el puntero de copia en la operacion de arrastrar y soltar
+                e.Effect = DragDropEffects.Copy;
+            } else
+            {
+                //Se muestra el puntero normal
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        //Se produce cuando se inicia una operacion de arrastrar y colocar
+        private void picBoxIzq_DragDrop(object sender, DragEventArgs e)
+        {
+            //Hace que se pegue la imagen en el otro pictureBox
+            PictureBox pictureBox = (PictureBox)sender;
+            pictureBox.Image = (Image)e.Data.GetData(DataFormats.Bitmap);
+        }
+    }
+}
